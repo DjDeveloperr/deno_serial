a6f6015f19341de0f9c91fc07dbe1eb1d67e56ac
Dj <43033058+DjDeveloperr@users.noreply.github.com>
2023-01-21 22:28:58 +0530

    fix: update local import

--- src/windows/deps.ts
+++ src/windows/deps.ts
@@ -4,4 +4,4 @@ export * as Comm from "https://win32.deno.dev/18563d7/Devices.Communication";
 export * as Reg from "https://win32.deno.dev/18563d7/System.Registry";
 export * as Foundation from "https://win32.deno.dev/18563d7/Foundation";
 export { OverlappedPromise } from "https://win32.deno.dev/18563d7/overlapped";
-export { unwrap } from "../../../deno_win32/error.ts";
+export { unwrap } from "https://raw.githubusercotntent.com/DjDeveloperr/deno_win32/18563d7/error.ts";
